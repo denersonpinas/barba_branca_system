f383da9f82c2a7e6885be26bf7611b7ee94b3cc3
denersonpinas <denersonpinas19@gmail.com>
2024-10-09 00:16:21 -0300

    feat: work in progress...

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 
 interface ITable {
   headers: string[]
-  data: any[]
+  data: never[]
 }
 
 const Table = ({ headers, data }: ITable) => {
   return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full table-auto">
-        <thead className="">
-          <tr className="border-b border-gray-400">
+    <div className='overflow-x-auto'>
+      <table className='min-w-full table-auto'>
+        <thead className=''>
+          <tr className='border-b border-gray-400'>
             {headers.map((header, index) => (
-              <th key={index} className="px-4 py-2 text-left font-semibold text-black">
+              <th
+                key={index}
+                className='px-4 py-2 text-left font-semibold text-black'
+              >
                 {formatString(header)}
               </th>
             ))}
@@ -21,9 +24,9 @@ const Table = ({ headers, data }: ITable) => {
         </thead>
         <tbody>
           {data.map((row, rowIndex) => (
-            <tr key={rowIndex} className="border-b">
+            <tr key={rowIndex} className='border-b'>
               {headers.map((header, index) => (
-                <td key={index} className="px-4 py-2 text-gray-600">
+                <td key={index} className='px-4 py-2 text-gray-600'>
                   {row[header]}
                 </td>
               ))}
